5ba44a84361dbf0d04ff2d7675495f280744f3a0
Buxta <robertpdavis@optusnet.com.au>
2022-05-12 23:22:47 +1000

    Created database handler class and created list employees query. Also installed dotenv to handle database connection details.

---
+++ server.js
@@ -0,0 +1,11 @@
+// Import and require mysql2
+const mysql = require('mysql2');
+// Import and require dotenv
+require('dotenv').config();
+
+const DB = require("./lib/DB");
+const db = new DB;
+
+const tableName = 'role';
+
+db.listEmployees();
