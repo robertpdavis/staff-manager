4fda643a429c7c97ac6779556937a7ae15e1dd58
Buxta <robertpdavis@optusnet.com.au>
2022-05-17 00:02:28 +1000

    Refactored index.js and Database class to use async flow and reduced duplication.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const inquirer = require('inquirer');
 // Import and require dotenv
 require('dotenv').config();
 //Import inquirer questions class
-const InqQuestions = require('./lib/InqQuestions');
-const questions = new InqQuestions;
+const Questions = require('./lib/Questions');
+const questions = new Questions;
 //Create database instance and get connection
 const Database = require("./lib/Database");
 const database = new Database;
@@ -12,200 +12,167 @@ const db = database.connection;
 // Import and require console.table
 const cTable = require('console.table');
 
-function prompter(action, questionList) {
+async function prompter(action, questionList) {
 
   const queries = database.getQueries();
-  var data;
+
+  let data = [];
+  let rows;
+  let obj = {};
+  let response;
 
   if (action === 'options' || questionList === "" || questionList === undefined) {
     questionList = questions.getQuestion('options');
   }
 
   if (questionList != "" && questionList != undefined) {
-    inquirer
+
+    const answers = await inquirer
       .prompt(questionList)
       .then((answers) => {
-
-        if (answers.options) {
-          switch (answers.options) {
-            case ("View Departments"):
-            case ("View Roles"):
-            case ("View Employees"):
-
-              let option = answers.options.split(" ")[1];
-              console.log(option);
-              db
-                .then(conn => conn.query(queries[`list${option}`]))
-                .then(([rows, fields]) => {
-                  console.log(`\nList of ${option}`);
-                  console.table(rows);
-                  prompter('options');
-                })
-                .catch(err => {
-                  console.log(err);
-                });
-              break;
-            case "Add Department":
-              questionList = questions.getQuestion('createDepartment');
-              console.log("Add a new department...");
-              prompter('createDepartment', questionList);
-              break;
-
-            case "Add Role":
-              //Get list of departments for question
-              db
-                .then(conn => conn.query(queries['listDepartments']))
-                .then(([rows, fields]) => {
-                  data = [];
-                  rows.forEach(element => {
-                    const obj = { name: element['Department Name'], value: element['Id'] };
-                    data.push(obj);
-                  });
-                  questionList = questions.getQuestion('createRole', data);
-                  console.log("Add a new role...");
-                  prompter('createRole', questionList);
-                })
-                .catch(err => {
-                  console.log(err);
-                });
-              break;
-
-            case "Add Employee":
-              //Get list of roles for question
-              data = [];
-              data[0] = [];
-              data[1] = [];
-
-              db
-                .then(conn => conn.query(queries['listRoles']))
-                .then(([rows, fields]) => {
-                  rows.forEach(element => {
-                    const obj = { name: element['Job Title'], value: element['Id'] };
-                    data[0].push(obj);
-                  })
-                })
-                .catch(err => {
-                  console.log(err);
-                });
-
-              db
-                .then(conn => conn.query(queries['listEmployees']))
-                .then(([rows, fields]) => {
-                  rows.forEach(element => {
-                    const obj = { name: (element['First Name'] + " " + element['Last Name']), value: element['Id'] };
-                    data[1].push(obj);
-                  });
-                  const obj = { name: 'None', value: null };
-                  data[1].push(obj);
-
-                  questionList = questions.getQuestion('createEmployee', data);
-                  console.log("Add a new employee...");
-                  prompter('createEmployee', questionList);
-                })
-                .catch(err => {
-                  console.log(err);
-                });
-              break;
-
-            case "Update Employee Role":
-              data = [];
-              data[0] = [];
-              data[1] = [];
-
-              db
-                .then(conn => conn.query(queries['listRoles']))
-                .then(([rows, fields]) => {
-                  rows.forEach(element => {
-                    const obj = { name: element['Job Title'], value: element['Id'] };
-                    data[0].push(obj);
-                  })
-                })
-                .catch(err => {
-                  console.log(err);
-                });
-
-              db
-                .then(conn => conn.query(queries['listEmployees']))
-                .then(([rows, fields]) => {
-                  rows.forEach(element => {
-                    const obj = { name: (element['First Name'] + " " + element['Last Name']), value: element['Id'] };
-                    data[1].push(obj);
-                  });
-                  const obj = { name: 'None', value: null };
-                  data[1].push(obj);
-
-                  questionList = questions.getQuestion('updateEmployeeRole', data);
-                  console.log("Update an employee...");
-                  prompter('updateEmployeeRole', questionList);
-                })
-                .catch(err => {
-                  console.log(err);
-                });
-              break;
-
-            case "Exit":
-              process.exit(0);
-              break;
-
-            default:
-              prompter('options');
-              break;
-          }
-        } else {
-          switch (action) {
-            case "createDepartment":
-              db
-                .then(conn => conn.query(queries['createDepartment'], [answers.departmentName]))
-                .then(([rows, fields]) => {
-                  console.log(`Department ${answers.departmentName} added.\n`);
-                  prompter('options');
-                })
-                .catch(err => {
-                  console.log(err);
-                });
-              break;
-            case "createRole":
-              db
-                .then(conn => conn.query(queries['createRole'], [answers.roleName, answers.roleSalary, answers.departmentChoice]))
-                .then(([rows, fields]) => {
-                  console.log(`Department ${answers.roleName} added.\n`);
-                  prompter('options');
-                })
-                .catch(err => {
-                  console.log(err);
-                });
-              break;
-            case "createEmployee":
-              db
-                .then(conn => conn.query(queries['createEmployee'], [answers.firstName, answers.lastName, answers.roleChoice, answers.employee]))
-                .then(([rows, fields]) => {
-                  console.log(`Employee ${answers.firstName} ${answers.lastName} updated.\n`);
-                  prompter('options');
-                })
-                .catch(err => {
-                  console.log(err);
-                });
-              break;
-            case "updateEmployeeRole":
-              db
-                .then(conn => conn.query(queries['updateEmployeeRole'], [answers.roleChoice, answers.employee]))
-                .then(([rows, fields]) => {
-                  console.log(`Employee updated.\n`);
-                  prompter('options');
-                })
-                .catch(err => {
-                  console.log(err);
-                });
-              break;
-
-            default:
-              break;
-          }
-        }
+        return answers;
       })
       .catch((error) => {
         console.log(error);
       });
+
+    if (answers.options) {
+      switch (answers.options) {
+        case ("View Departments"):
+        case ("View Roles"):
+        case ("View Employees"):
+
+          let option = answers.options.split(" ")[1];
+          rows = await database.runQuery(`list${option}`, "");
+
+          console.log(`\nList of ${option}`);
+          console.table(rows);
+          prompter('options');
+
+          break;
+        case "Add Department":
+          questionList = questions.getQuestion('createDepartment');
+          console.log("Add a new department...");
+          prompter('createDepartment', questionList);
+          break;
+
+        case "Add Role":
+          //Get list of departments for question
+          rows = await database.runQuery('listDepartments', "");
+
+          rows.forEach(element => {
+            obj = { name: element['Department Name'], value: element['Id'] };
+            data.push(obj);
+          });
+
+          questionList = questions.getQuestion('createRole', data);
+          console.log("Add a new role...");
+          prompter('createRole', questionList);
+
+          break;
+
+        case "Add Employee":
+          //Get list of roles for question
+          data = [];
+          data[0] = [];
+          data[1] = [];
+
+          rows = await database.runQuery('listRoles', "");
+          rows.forEach(element => {
+            obj = { name: element['Job Title'], value: element['Id'] };
+            data[0].push(obj);
+          });
+
+          rows = await database.runQuery('listEmployees', "");
+          rows.forEach(element => {
+            obj = { name: (element['First Name'] + " " + element['Last Name']), value: element['Id'] };
+            data[1].push(obj);
+          });
+          obj = { name: 'None', value: null };
+          data[1].push(obj);
+
+          questionList = questions.getQuestion('createEmployee', data);
+          console.log("Add a new employee...");
+          prompter('createEmployee', questionList);
+
+          break;
+
+        case "Update Employee Role":
+          data = [];
+          data[0] = [];
+          data[1] = [];
+
+          rows = await database.runQuery('listRoles', "");
+          rows.forEach(element => {
+            obj = { name: element['Job Title'], value: element['Id'] };
+            data[0].push(obj);
+          });
+
+          rows = await database.runQuery('listEmployees', "");
+          rows.forEach(element => {
+            obj = { name: (element['First Name'] + " " + element['Last Name']), value: element['Id'] };
+            data[1].push(obj);
+          });
+          obj = { name: 'None', value: null };
+          data[1].push(obj);
+
+          questionList = questions.getQuestion('updateEmployeeRole', data);
+          console.log("Update an employee...");
+          prompter('updateEmployeeRole', questionList);
+
+          break;
+
+        case "Exit":
+          process.exit(0);
+          break;
+
+        default:
+          prompter('options');
+          break;
+      }
+    } else {
+      switch (action) {
+        case "createDepartment":
+
+          response = await database.runQuery(`createDepartment`, [answers.departmentName]);
+
+          if (response['affectedRows'] > 0) {
+            console.log(`Department ${answers.departmentName} added.\n`);
+          }
+
+          prompter('options');
+
+          break;
+        case "createRole":
+
+          response = await database.runQuery(`createRole`, [answers.roleName, answers.roleSalary, answers.departmentChoice]);
+          if (response['affectedRows'] > 0) {
+            console.log(`Role ${answers.roleName} added.\n`);
+          }
+          prompter('options');
+
+          break;
+        case "createEmployee":
+          response = await database.runQuery(`createEmployee`, [answers.firstName, answers.lastName, answers.roleChoice, answers.employee]);
+          if (response['affectedRows'] > 0) {
+            console.log(`Employee ${answers.firstName} ${answers.lastName} updated.\n`);
+          }
+          prompter('options');
+
+          break;
+        case "updateEmployeeRole":
+          response = await database.runQuery(`updateEmployeeRole`, [answers.roleChoice, answers.employee]);
+          if (response['affectedRows'] > 0) {
+            console.log(`Employee updated.\n`);
+          }
+          prompter('options');
+
+          break;
+
+        default:
+          break;
+      }
+    }
   } else {
     console.log("No question list defined. Reseting...");
     prompter('options');
@@ -214,3 +181,4 @@ function prompter(action, questionList) {
 }
 
 prompter('options');
+// test();
